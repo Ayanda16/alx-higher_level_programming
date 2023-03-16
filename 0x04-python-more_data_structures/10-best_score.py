e33ea2496d610b27ff4da2f42a4a9ceb2afd8f30
Ayanda16 <ntselea16@gmail.com>
2023-03-17 00:56:25 +0200

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
 
-def multiply_by_2(a_dictionary):
-    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
+    result = list(a_dictionary.keys())[0]
+    best_score = list(a_dictionary.values())[0]
+    for k, v in a_dictionary.items():
+        if v > best_score:
+            best_score = v
+            result = k
+    return (result)

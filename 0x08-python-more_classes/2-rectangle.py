c0b989f27083f96ce36fdde1d0b7d88f9b6f13df
Ayanda16 <ntselea16@gmail.com>
2023-04-07 01:38:28 -0700

    pycodestyle correction

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle():
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         """retrieve the height of the rectangle"""

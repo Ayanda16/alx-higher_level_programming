52739a1d204c8ae89e191b497b329e83d8e48e94
Ayanda16 <ntselea16@gmail.com>
2023-04-09 13:33:34 -0700

    compare rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -46,6 +46,17 @@ class Rectangle:
         """returns the rectangle area"""
         return (self.__width * self.__height)
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """" returns the biggest rectangle based on the area"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() == rect_2.area():
+            return rect_1
+        return rect_2
+
     def perimeter(self):
         """return the rectangle parametere"""
         if self.__width == 0 or self.__height == 0:

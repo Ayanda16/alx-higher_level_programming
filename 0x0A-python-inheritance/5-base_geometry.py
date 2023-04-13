608a042974d6c01f4757841746b5a3364703d7f4
Ayanda16 <ntselea16@gmail.com>
2023-04-13 13:53:48 -0700

    improve geometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" create an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" improve geometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """public instance method"""
+        raise Exception("area() is not implemented")

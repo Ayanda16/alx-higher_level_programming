900c443194997f7c887bf7e51bd89cc952ef5913
Ayanda16 <ntselea16@gmail.com>
2023-04-13 12:44:00 -0700

    change from isinstance to type

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,6 +6,6 @@
 def is_same_class(obj, a_class):
     """fuction that returns true if the object is exactly an instance
     of the specified class, otherwise false"""
-    if not isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
     return False

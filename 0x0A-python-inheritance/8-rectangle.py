e8f8fe2b7a68d142e5915033856d322a36bfc926
Ayanda16 <ntselea16@gmail.com>
2023-04-13 14:20:53 -0700

    rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Integer validator"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """public instance method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """public instance method that validates value to be an int type"""
+
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """class Rectangle inherited from class BaseGeometry"""
+
+    def __init__(self, width, height):
+        """ initialisation of class Rectangle"""
+
+        self.width = width
+        self.height = height

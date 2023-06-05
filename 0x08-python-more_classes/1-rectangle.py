6d57ce4fcbc9b02c8c230277a55b913d0cf882b4
Ayanda16 <ntselea16@gmail.com>
2023-06-05 11:37:57 -0700

    define rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""defines a class rectangle"""
+
+
+class Rectangle:
+    """represents a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """initialisaton of rectangle
+           width: width of the rectangle
+           height: height of the rectangle""" 
+    
+    @property
+    def width(self):
+        """get width of the rectangle"""

53e5bd34f736c256dabc09984491027b5155aa86
Ayanda16 <ntselea16@gmail.com>
2023-06-20 09:50:31 -0700

    fixing height getter

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -28,7 +28,7 @@ class Rectangle(Base):
         self.__width = value
 
     @property
-    def height(self, value):
+    def height(self):
         """get height of rectangle"""
         return self.__height
 
